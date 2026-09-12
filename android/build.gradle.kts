364b82cba36a219896f8b7fca39acec05e605eb8
Ramli Sarif <ramli.sarif@gmail.com>
2026-09-12 19:25:01 +0900

    update 1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,8 +17,30 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    // if (project.hasProperty('android')) {
+    //         project.android {
+    //             compileSdkVersion 36
+    //         }
 }
+
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+subprojects {
+    val configureProject = {
+        if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
+            }
+        }
+    }
+
+    if (state.executed) {
+        configureProject()
+    } else {
+        afterEvaluate {
+            configureProject()
+        }
+    }
 }
--- lib/firebase_option.dart
+++ lib/firebase_option.dart
@@ -6,7 +6,7 @@ class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       return web;
-    }
+    } 
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
